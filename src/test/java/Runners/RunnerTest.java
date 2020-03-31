5868b07e2adc89f744f331d8ae77bb343ce56fcb
anujagarg90 <anuja.garg990@gmail.com>
2020-03-31 16:37:37 +0530

    First Commit

---
+++ src/test/java/Runners/RunnerTest.java
@@ -0,0 +1,10 @@
+package Runners;
+
+public class RunnerTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello From Test Code");
+	}
+
+}
